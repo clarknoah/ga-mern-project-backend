f2a49ffbcb98d1ba97613cb0619c044ea5c774c4
Jenna Dean <jenna.dean9@gmail.com>
2019-11-19 10:14:12 -0500

    dummy comment

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ const app = express();
 const User = require("../db/models/User.js");
 const bodyParser = require("body-parser");
 
-const routes = require('./routes/index.js');
+const routes = require("./routes/index.js");
 
 app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
 
-app.use(express.static('doc'));
-
+app.use(express.static("doc"));
 
 /**
  * @api {get} / Test API
@@ -21,47 +20,39 @@ app.use(express.static('doc'));
  * @apiSuccess {String} count Says Hello API
  */
 
-
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Hello API");
-})
+});
 
-
-app.post('/login',(req,res)=>{
+app.post("/login", (req, res) => {
   let user = req.body;
-  User.find(user)
-    .then(result=>{
-      console.log(result);
-      res.json(result);
-    })
-})
+  User.find(user).then(result => {
+    console.log(result);
+    res.json(result);
+  });
+});
 
-
-app.post('/user',(req,res)=>{
+app.post("/user", (req, res) => {
   let user = req.body;
-  User.create(user)
-    .then(result=>{
-      console.log(result);
-      res.json(result);
-    })
-})
+  User.create(user).then(result => {
+    console.log(result);
+    res.json(result);
+  });
+});
 
-
-app.get('/user/:handle',(req,res)=>{
+app.get("/user/:handle", (req, res) => {
   let handle = req.params.handle;
   let user = req.body;
-  User.find({handle:handle})
-    .then(result=>{
-      console.log(result);
-      res.json(result);
-    })
-})
-
-
-
+  User.find({ handle: handle }).then(result => {
+    console.log(result);
+    res.json(result);
+  });
+});
 
 app.set("port", process.env.PORT || 5555);
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
+
+//this is my dummy comment to test merge-ability from tweeproutes branch
