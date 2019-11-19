5a198c1f9983e9f88c9e5e4fd28e93809dee6930
Jenna Dean <jenna.dean9@gmail.com>
2019-11-19 12:53:20 -0500

    fixed routing error

--- src/controllers/tweeps.js
+++ src/controllers/tweeps.js
@@ -2,6 +2,17 @@ const User = require("../../db/models/User");
 
 const ctrl = {
   createTweep: (req, res) => {
+    User.findOneAndUpdate(
+      { handle: req.params.handle },
+      {
+        $push: {
+          tweeps: req.body
+        }
+      },
+      { new: true }
+    ).then(tweep => {
+      res.json(tweeps);
+    });
     console.log(req);
   }
 };
@@ -9,3 +20,20 @@ const ctrl = {
 module.exports = ctrl;
 
 //dummy comment from tweeproutes branch
+
+// Users.findOneAndUpdate({ name: req.user.name }, { $push: { friends: friend } });
+// };
+
+// SuperBowl.findOneAndUpdate(
+//   { super_bowl: performance.super_bowl },
+//   {
+//     $push: {
+//       halftimePerformer: {
+//         super_bowl: performance.super_bowl,
+//         musician: performance.musician,
+//         num_songs: performance.num_songs
+//       }
+//     }
+//   },
+//   { new: true }
+// );
