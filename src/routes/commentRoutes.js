18efd6adcbeb53b4a758320820aa236b7be52744
joshseven <neves.joshua@gmail.com>
2019-11-19 10:10:25 -0500

    comment dummy

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 const ctrl = require('../controllers/comments.js');
 
-
+// Test 
 router.get('/user/:handle/tweeps/:tId/comments', ctrl.createComment);
 
 module.exports = router;
