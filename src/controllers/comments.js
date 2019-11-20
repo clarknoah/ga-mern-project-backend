adfce53a06aa9f239b7492ed14e0fd8fe726aa54
joshseven <neves.joshua@gmail.com>
2019-11-20 09:40:08 -0500

    comments

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -3,8 +3,18 @@ const User = require("../../db/models/User");
 
 const ctrl = {
   createComment: (req, res) => {
+    User.findOneAndUpdate(
+      { handle: req.params.handle },
+      {
+        $push: {
+          tweeps: req.body
+        }
+      },
+      { new: true }
+    ).then(comment => {
+      res.json(comment);
+    });
     console.log(req);
   }
-}
-
+};
 module.exports = ctrl;
